f105054cf3aeca0351a6fb7cf7653498178fb1ba
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-02-01 08:43:22 +0530

    Added email model with data annotation To validate a lsit of email custom data attribute is added

---
+++ SMTP.Service/Model/CustomAttributes/EmailAddressListValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMTP.Service.Model.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class IsListOfEmailAddress : DataTypeAttribute
+    {
+        #region privates
+        private readonly EmailAddressAttribute _emailAddressAttribute = new EmailAddressAttribute();
+        #endregion
+
+        #region ctor
+        public IsListOfEmailAddress() : base(DataType.EmailAddress) { }
+        #endregion
+
+        #region Overrides
+        /// <summary>
+        /// Checks if the value is valid
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override bool IsValid(object value)
+        {
+            var emailAddressList = value as List<string>;
+            if (emailAddressList != null) return false;
+
+            //lets test for mulitple email addresses
+            
+             return emailAddressList.All(t => _emailAddressAttribute.IsValid(t));
+        }
+        #endregion
+
+    }
+}
---
+++ SMTP.Service/Model/MailModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using SMTP.Service.Model.CustomAttributes;
+
+namespace SMTP.Service.Model
+{
+    public class MailModel
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="From Address cannot be empty")]
+        [EmailAddress(ErrorMessage ="Invalid from address email")]
+        public string FromAddress { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "To Address cannot be empty")]
+        [IsListOfEmailAddress(ErrorMessage ="One or some of the email provided in the To address list is invalid")]
+        public List<string> ToAddress { get; set; }
+        [IsListOfEmailAddress(ErrorMessage = "One or some of the email provided in the To address list is invalid")]
+        public List<string> Bcc { get; set; }
+        [IsListOfEmailAddress(ErrorMessage = "One or some of the email provided in the To address list is invalid")]
+        public List<string> Cc { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Subject cannot be empty")]
+        public string Subject { get; set; }
+
+        public string  Body { get; set; }
+    }
+}
