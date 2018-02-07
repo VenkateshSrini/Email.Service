4229d23a39b66a9b1576e81fe9af6678dbca92bf
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-02-07 09:25:05 +0530

    coded the integration test.Need to run and test

--- test/SMTP.Service.Integration.Test/Controllers/MailController_Test.cs
+++ test/SMTP.Service.Integration.Test/Controllers/MailController_Test.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using SMTP.Service.Model;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using Xunit;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace SMTP.Service.Integration.Test.Controllers
 {
-    class MailController_Test
+    public class MailController_Test:TestBase
     {
+        private HttpClient httpClient;
+        public MailController_Test()
+        {
+            httpClient = testServer.CreateClient();
+        }
+        [Fact]
+        public async void TestPost()
+        {
+            MailModel mailModel = new MailModel
+            {
+                FromAddress="svenkatesh_in@yahoo.com",
+                ToAddress = new List<string>
+                {
+                    "venkateshsrini3@gmail.com",
+                    "heman_1978@hotmail.com"
+                },
+                Subject = "Hi there",
+                Body ="<html><head>issue</head></html>"
+            };
+
+            var response = await httpClient.PostAsync("/api/Mail", new StringContent(JsonConvert.SerializeObject(mailModel),
+                                                                System.Text.Encoding.UTF8,
+                                                                "application/json"
+                                                                )
+                                 );
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
     }
 }
